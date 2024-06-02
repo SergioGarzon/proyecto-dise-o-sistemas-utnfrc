a395fef422b2c519ec3e8f1119ad11e6a945b7e0
Sergio Garzon <sggarzonutn@gmail.com>
2024-06-02 18:06:30 -0300

    Primer commit

---
+++ src/main/java/edu/utnfrc/ppai_diseno_siistemas_utn_frc/PpaiDisenoSiistemasUtnFrcApplication.java
@@ -0,0 +1,13 @@
+package edu.utnfrc.ppai_diseno_siistemas_utn_frc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpaiDisenoSiistemasUtnFrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpaiDisenoSiistemasUtnFrcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utnfrc/ppai_diseno_siistemas_utn_frc/PpaiDisenoSiistemasUtnFrcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utnfrc.ppai_diseno_siistemas_utn_frc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpaiDisenoSiistemasUtnFrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
